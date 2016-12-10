730ba9474b4b6189b73f8754a1ecaf4bdcd0fb2d
wen <wen@wens-mbp.attlocal.net>
2016-12-09 22:38:30 -0600

    dinnerspinner

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -3,10 +3,10 @@ var app = express()
 
 var Yelp = require('yelp');
 var yelp = new Yelp({
-	consumer_key: "QX6AnsA4NRlYlCLUVFziIw",
-	consumer_secret: "Rr8vgooT0PXeq_0H4bXFppd_nsY",
-	token: "ZT3pl3vqniQ_Q8V40yf07l5FXRxY-fQt",
-	token_secret: "o1gvIqxMeJx0gXQrzNkkleOKEKE"
+	consumer_key: "ygFawGgchLPiVcFVWN8nKQ",
+	consumer_secret: "kM5LMHLL_OLe2N9NeenSgQ2bpmg",
+	token: "_L4v1t6kMsRuB92hhUNlI7xDCQdhH6Cz",
+	token_secret: "G-kkHPcb5BRt60VdusOV7C8PCLE"
 });
 
 app.get('/', function (req, res) {
